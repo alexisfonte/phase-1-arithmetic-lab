4a5baab2536e0e6497a1457edde46d1f6ee64904
Alexis Fonte <alexis.e.fonte@gmail.com>
2022-11-21 21:48:19 -0600

    commit tree 7c640403b823fad56f252647f75b2f1f9cadd579

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2;
 
-// Write your code here
+let num2 = 31;
+
+const multiply = num1 * num2;
+
+const random = Math.round(Math.random() * 10) + 1;
+
+let num3 = 17;
+
+let num4 = 13;
+
+const mod = num3 % num4;
+
+const max = Math.max(4, 8, 12, 16, 20);
